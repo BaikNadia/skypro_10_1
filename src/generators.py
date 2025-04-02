48866896a9d7afe164551c7f026183fb6d8c2c6b
BaikNadia <baikowa.nadia@gmail.com>
2025-04-02 22:37:49 +0300

    change generators

--- src/generators.py
+++ src/generators.py
@@ -29,19 +29,14 @@ def card_number_generator(start=1, end=9999999999999999):
     """
     Генерирует номера банковских карт в формате XXXX XXXX XXXX XXXX.
 
-    :param start: Начальное значение диапазона (по умолчанию 1).
-    :param end: Конечное значение диапазона (по умолчанию 9999999999999999).
+    :param start: Начальное значение диапазона.
+    :param end: Конечное значение диапазона.
     :yield: Номер карты в формате XXXX XXXX XXXX XXXX.
     """
-    # Проверяем корректность диапазона
     if start < 1 or end > 9999999999999999 or start > end:
         raise ValueError("Неверный диапазон для номеров карт")
 
     for number in range(start, end + 1):
-        # Форматируем число в строку длиной 16 символов с нулями слева
-        formatted_number = f"{number:016d}"
-
-        # Разбиваем строку на группы по 4 цифры и объединяем через пробелы
-        card_number = " ".join([formatted_number[i:i + 4] for i in range(0, 16, 4)])
-
+        formatted_number = f"{number:016d}"  # Добавляем нули слева до 16 цифр
+        card_number = " ".join([formatted_number[i:i+4] for i in range(0, 16, 4)])  # Форматируем как XXXX XXXX XXXX XXXX
         yield card_number
