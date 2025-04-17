bc039856ddb46a4570941b0f693a7fd065c5d6a6
BaikNadia <baikowa.nadia@gmail.com>
2025-04-17 18:28:04 +0300

    change logging

--- src/logging_config.py
+++ src/logging_config.py
@@ -3,29 +3,23 @@ import os
 
 def setup_logger(module_name: str):
     """
-    Настройка логгера для указанного модуля.
+    Настройка логгера для указанного модуля с уровнем логирования DEBUG.
 
     :param module_name: Название модуля (например, 'masks' или 'utils').
     :return: Logger для указанного модуля.
     """
-    # Создаем директорию logs, если она не существует
     log_dir = "logs"
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
-    # Имя файла лога
     log_file = f"{log_dir}/{module_name}.log"
 
-    # Создаем логгер
     logger = logging.getLogger(module_name)
-    logger.setLevel(logging.INFO)  # Устанавливаем уровень логирования
+    logger.setLevel(logging.DEBUG)  # Устанавливаем уровень логирования как DEBUG
 
-    # Создаем обработчик для записи в файл
-    file_handler = logging.FileHandler(log_file, mode="w", encoding="utf-8")  # Перезаписываем файл при каждом запуске
+    file_handler = logging.FileHandler(log_file, mode="w", encoding="utf-8")
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
 
-    # Добавляем обработчик к логгеру
     logger.addHandler(file_handler)
-
     return logger
